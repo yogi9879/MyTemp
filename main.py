19b5bb0ddb02da9284fa49ee8051b3173dc27cc7
yogi9879 <32189018+yogi9879@users.noreply.github.com>
2018-10-23 13:36:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ Created on Sun Oct 21 10:02:30 2018
 @author: yogesh
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template #, request,make_response
 app = Flask(__name__)
  
 @app.route("/")
 def index():
     return render_template('upload.html')
  
-@app.route("/hello")
+@app.route("/hello",methods = ['POST', 'GET'])
 def hello():
     return "Hello World!"
  
