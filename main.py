af6b7e514cc56d43fdbfdbc88a9905c4b5195030
yogesh <nakhateyogesh@gmail.com>
2018-10-24 00:13:00 +0530

    adding all

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 21 10:02:30 2018
+
+@author: yogesh
+"""
+
+from flask import Flask, render_template
+app = Flask(__name__)
+ 
+@app.route("/")
+def index():
+    return render_template('upload.html')
+ 
+@app.route("/hello")
+def hello():
+    return "Hello World!"
+ 
+ 
+if __name__ == "__main__":
+    app.run()
