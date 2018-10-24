d6faf3136fdb7a7d4d7a6e91ac7519bef5eb5974
yogi9879 <32189018+yogi9879@users.noreply.github.com>
2018-10-24 16:13:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import flask
 #import numpy 
 #from sklearn.svm import SVR
 #from sklearn.cross_validation import cross_val_score
-import sub
+#import sub
 
 #from flask import render_template
 #from FlaskWeb import app
