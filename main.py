e8e2292ecc037c00aa1d8bb0f08975a16a095a4e
yogi9879 <32189018+yogi9879@users.noreply.github.com>
2018-10-24 14:06:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from flask import Flask, render_template, request, make_response
 from datetime import datetime
 from werkzeug import secure_filename
 from werkzeug.datastructures import ImmutableMultiDict
-import pandas as pd
+#import pandas as pd
 #import matplotlib.pyplot as plt
 #import numpy as np
 #from sklearn.svm import SVR
