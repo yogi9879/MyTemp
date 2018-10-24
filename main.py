a314a98dab9705ca67a23b60556fd2de4f5d1c6b
yogi9879 <32189018+yogi9879@users.noreply.github.com>
2018-10-24 12:58:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ from flask import Flask, render_template, request, make_response
 from datetime import datetime
 from werkzeug import secure_filename
 from werkzeug.datastructures import ImmutableMultiDict
-import pycode.myfile
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.svm import SVR
+from sklearn.cross_validation import cross_val_score
 
 #from flask import render_template
 #from FlaskWeb import app
@@ -28,6 +32,12 @@ def hello():
     #X=request.form
     f = request.files['file']
     return "file uploaded"               #render_template('index.html',name=X)
+ 
+
+ 
+
+ 
+ 
  
 if __name__ == "__main__":
     app.run()
