50591033b9e5bb4063647e3038b40d3d90a64ad6
yogi9879 <32189018+yogi9879@users.noreply.github.com>
2018-10-24 16:17:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from flask import Flask, render_template, request, make_response
 from datetime import datetime
 from werkzeug import secure_filename
 from werkzeug.datastructures import ImmutableMultiDict
-import flask
+#import flask
 #import matplotlib.pyplot as plt
 #import numpy 
 #from sklearn.svm import SVR
@@ -32,7 +32,7 @@ def hello():
  if request.method == 'POST':
     #X=request.form
     f = request.files['file']
-    dataset=pd.read_csv(f)
+ #   dataset=pd.read_csv(f)
     #results=sub.Model(dataset)
     return "HI"             #render_template('index.html',name=X)
  
