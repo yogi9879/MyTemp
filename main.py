6f171f3aad914454314d2b1b11ba31f1f6be3152
yogi9879 <32189018+yogi9879@users.noreply.github.com>
2018-10-25 22:17:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ Created on Sun Oct 21 10:02:30 2018
 @author: yogesh
 """
 
-from flask import Flask, render_template#, request, make_response
-#from datetime import datetime
-#from werkzeug import secure_filename
-#from werkzeug.datastructures import ImmutableMultiDict
+from flask import Flask, render_template,request, make_response
+from datetime import datetime
+from werkzeug import secure_filename
+from werkzeug.datastructures import ImmutableMultiDict
 #import flask
 #import matplotlib.pyplot as plt
 #import numpy 
@@ -22,8 +22,14 @@ import sub
 
 app = Flask(__name__)
 
+@app.route('/',methods = ['POST', 'GET'])
+def result():
+  
+      return render_template("upload.html")
+
+
  
-@app.route("/")
+@app.route("hello/")
 def index():
     return "gikgig" # render_template('upload1.html')
  
